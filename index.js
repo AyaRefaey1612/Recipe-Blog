63d4f1c65f12acf796928ff4750d09f76243bec1
Aya <arefaey787@gmail.com>
2025-02-09 20:32:51 +0200

    edit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 4000;
 const mongoUrl = process.env.MONGO_URI;
 
 // middlewares
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(expressLayout);
